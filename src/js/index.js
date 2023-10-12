d1172ce4de4574cd6fa908d823c9ebf5212c42cf
= <gui.silva93@hotmail.com>
2023-10-12 11:25:04 -0300

    commit

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+const inputs = document.querySelectorAll('.form-field')
+const mandatory = document.querySelectorAll('.mandatory-hidden')
+const btnSend = document.getElementById('send-button')
+const form = document.getElementById('form')
+
+
+console.log(btnSend);
+
+btnSend.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    let i = 0
+    const countInput = inputs.length
+
+    inputs.forEach(function(formField) {
+
+        if (formField.value === '') {
+            formField.classList.remove('ready')
+            formField.classList.add('needed')
+
+            formField.nextElementSibling.classList.add('visible')
+
+            
+        } else{
+            formField.classList.add('ready')
+
+            formField.nextElementSibling.classList.remove('visible')
+            i++
+        }
+
+        if(i === countInput) {
+            form.submit()
+        } else {
+            alert('Preencha todos os campos')
+            return 
+        }
+    });
+});
